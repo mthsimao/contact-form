105fbc20e51bb0a5342cd3e8ee57f9cba1daba65
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2024-08-15 16:44:14 -0300

    form validation pt2

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,14 @@ let divSucess = document.querySelector('.success-pop')
 const form = document.querySelector('.form');
 const campos = document.querySelectorAll('.box-required');
 const spans = document.querySelectorAll('.span-required');
-const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.[a-z]?$/i;
+const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+
+
 
 // name validate
 
 const nameValidate = () => {
-    if(campos[0].value.length < 3) {
+    if(campos[0].value.length < 3 || campos[0].value == ' ') {
         setError(0);
     } else {
         removeError(0);
@@ -19,7 +21,7 @@ const nameValidate = () => {
 // last name validate
 
 const lastNameValidate = () => {
-    if(campos[1].value.length < 3) {
+    if(campos[1].value.length < 3 || campos[1].value == ' ') {
         setError(1);
     } else {
         removeError(1);
@@ -30,10 +32,41 @@ const lastNameValidate = () => {
 // email validate
 
 const emailValidate = () => {
-    if(emailRegex.test(campos[2].value)) {
+    if(!emailRegex.test(campos[2].value)) {
+        setError(2);
+    } else if(campos[2].value == '') {
         setError(2);
     } else {
         removeError(2);
+    }
+}
+
+// input radios validation
+
+let querytype = document.querySelectorAll('.queryType1')
+
+
+const radioValidate = () => {
+    if(!campos[3].checked && !campos[4].checked) {
+        setError(3);
+    } else {
+        removeError(3);
+    }
+}
+
+// checkbox validate 
+
+const checkbox = document.querySelector('#icheckbox')
+
+checkbox.addEventListener('change', () => {
+    checkboxValidate();
+})
+
+const checkboxValidate = () => {
+    if(!checkbox.checked) {
+        spans[5].style.display = 'inline-block'
+    } else {
+        spans[5].style.display = 'none'
     }
 }
 
@@ -49,10 +82,12 @@ const removeError = (index) => {
     spans[index].style.display = 'none'
 }
  
-
-btn_submit.addEventListener('click', (event) => {
+form.addEventListener('submit', (event) => {
     event.preventDefault();
-    divSucess.classList.toggle('pop-up')
-
+    nameValidate();
+    lastNameValidate();
+    emailValidate();
+    radioValidate();
+    checkboxValidate();
+    divSucess.classList.add('pop-up')
 })
-
